0850b9f2bdaba498e8853a9a1ae3bb060547806e
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2017-10-24 22:27:12 -0300

    O código esta funcionando normalmente agora, o erro que persistia, era uma variavel que estava escrita incorretamente no lugar de ser (pointY) estava (pintY)

--- PontosCartesianos.py
+++ PontosCartesianos.py
@@ -1,17 +1,19 @@
-class point():
+class point(object):
 
     def __init__(self):
         self.pointX=None
-        self.pintY=None
+        self.pointY=None
 
-    def __add__(self, other):
-        return self+other
+    def __add__(self, novo):
+        pX=self.pointX+novo.pointX
+        pY=self.pointY+novo.pointY
+        return (pX,pY)
 
     def __str__(self):
         pontoX=str(self.pointX)
-        pontoY=str(self.pintY)
+        pontoY=str(self.pointY)
         print("ponto x:" + pontoX + " /n ponto y" + pontoY )
 
     def solicitandoPontos(self):
-        self.pointX=int(input("Por favor informe a posição do elemento em relação a X do plano"))
-        self.pintY=int(input("Por favor informe a posição do elemento em relação a Y do plano"))
+        self.pointX=int(input("Por favor informe a posição do elemento em relação a X do plano: "))
+        self.pointY=int(input("Por favor informe a posição do elemento em relação a Y do plano: "))
--- models/APP.py
+++ models/APP.py
@@ -1,11 +1,10 @@
 from PontosCartesianos import point
-def main(String = []):
+def main(args = []):
     p=point()
     p2=point()
-    p3=point()
     p.solicitandoPontos()
     p2.solicitandoPontos()
-    p3=p+p2
-    print(p3)
+    print(p+p2)
+
 if __name__=="__main__":
     main()
